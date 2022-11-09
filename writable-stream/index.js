58c4251bc3b2db63e5504fe92ceffd300568a142
permanayogi <yogipermana597@gmail.com>
2022-11-09 19:21:07 +0700

    write stream to output

---
+++ writable-stream/index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+const readableStream = fs.createReadStream("./input.txt", {
+  highWaterMark: 15,
+});
+
+const writableStream = fs.createWriteStream("output.txt");
+
+readableStream.on("readable", () => {
+  try {
+    writableStream.write(`${readableStream.read()}\n`);
+  } catch (error) {
+    return error.message;
+  }
+});
+
+readableStream.on("end", () => {
+  console.log("");
+});
