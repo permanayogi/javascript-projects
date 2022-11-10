0b4a6d3dc278f0b9635384be3564a63e68ddf1f6
permanayogi <yogipermana597@gmail.com>
2022-11-10 20:28:24 +0700

    routing request

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -7,36 +7,44 @@ const http = require("http");
  */
 
 const requestListener = (request, response) => {
-  response.setHeader("Content-Type", "text/html");
-  response.statusCode = 200;
+    response.setHeader("Content-Type", "text/html");
+    response.statusCode = 200;
 
-  const { method } = request;
+    const { method, url } = request;
 
-  //   if (method == "GET") {
-  //     response.end("<h1>Hello!</h1>");
-  //   }
+    if (url === "/") {
+        if (method == "GET") {
+            response.end("<h1>Ini adalah homepage</h1>");
+        } else {
+            response.end(
+                `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
+            );
+        }
+    } else if (url === "/about") {
+        if (method == "GET") {
+            response.end("<h1>Halo! Ini adalah halaman about</h1>");
+        }
 
-  if (method == "POST") {
-    let body = [];
+        if (method == "POST") {
+            let body = [];
 
-    request.on("data", (chunk) => {
-      body.push(chunk);
-    });
+            request.on("data", (chunk) => {
+                body.push(chunk);
+            });
 
-    request.on("end", () => {
-      body = Buffer.concat(body).toString();
-      const { name } = JSON.parse(body);
-      response.end(`<h1>Hai ${name}!</h1>`);
-    });
-  }
-
-  //   if (method == "PUT") {
-  //     response.end("<h1>Bonjour</h1>");
-  //   }
-
-  //   if (method == "DELETE") {
-  //     response.end("<h1>Salam!</h1>");
-  //   }
+            request.on("end", () => {
+                body = Buffer.concat(body).toString();
+                const { name } = JSON.parse(body);
+                response.end(`<h1>Hai ${name}! Ini adalah halaman about!</h1>`);
+            });
+        } else {
+            response.end(
+                `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
+            );
+        }
+    } else {
+        response.end("<h1>Halaman tidak ditemukan!</h1>");
+    }
 };
 
 const server = http.createServer(requestListener);
@@ -45,5 +53,5 @@ const port = 5000;
 const host = "localhost";
 
 server.listen(port, host, () => {
-  console.log(`Server berjalan pada http://${host}:${port}`);
+    console.log(`Server berjalan pada http://${host}:${port}`);
 });
