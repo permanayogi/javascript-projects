d9cb0a93a9dcc53da5d61973ee160c12fc339472
permanayogi <yogipermana597@gmail.com>
2022-11-10 20:49:38 +0700

    set status code on response

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -8,20 +8,23 @@ const http = require("http");
 
 const requestListener = (request, response) => {
     response.setHeader("Content-Type", "text/html");
-    response.statusCode = 200;
+    // response.statusCode = 200;
 
     const { method, url } = request;
 
     if (url === "/") {
         if (method == "GET") {
+            response.statusCode = 200;
             response.end("<h1>Ini adalah homepage</h1>");
         } else {
+            response.statusCode = 400;
             response.end(
                 `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
             );
         }
     } else if (url === "/about") {
         if (method == "GET") {
+            response.statusCode = 200;
             response.end("<h1>Halo! Ini adalah halaman about</h1>");
         }
 
@@ -35,14 +38,17 @@ const requestListener = (request, response) => {
             request.on("end", () => {
                 body = Buffer.concat(body).toString();
                 const { name } = JSON.parse(body);
+                response.statusCode = 200;
                 response.end(`<h1>Hai ${name}! Ini adalah halaman about!</h1>`);
             });
         } else {
+            response.statusCode = 400;
             response.end(
                 `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
             );
         }
     } else {
+        response.statusCode = 400;
         response.end("<h1>Halaman tidak ditemukan!</h1>");
     }
 };
