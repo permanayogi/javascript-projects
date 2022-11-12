4e25916651d699f9d379953fca7f5e264de3b9b9
permanayogi <yogipermana597@gmail.com>
2022-11-12 08:16:37 +0700

    http server

--- node-app-back-end/src/server.js
+++ node-app-back-end/src/server.js
@@ -1,4 +1,13 @@
-/* eslint-disable linebreak-style */
-const Hapi = require("@hapi/hapi");
+const Hapi = require('@hapi/hapi');
 
-const init = (async) => ({});
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
