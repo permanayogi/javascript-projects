9a758140e5cedef40df6ddf65cf2cc3f8841c1a9
permanayogi <yogipermana597@gmail.com>
2022-11-16 17:27:38 +0700

    add handler for insert and get all books

---
+++ bookshelf-api/src/handler.js
@@ -0,0 +1,81 @@
+/* eslint linebreak-style: ["error", "windows"] */
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+const addBooksHandler = (request, h) => {
+  const {
+    name, year, author, summary, publisher, pageCount, readPage, reading,
+  } = request.payload;
+  const id = nanoid(16);
+
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+  let finished;
+
+  if (pageCount === readPage) {
+    finished = true;
+  } else {
+    finished = false;
+  }
+
+  const newBooks = {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+    id,
+    finished,
+    createdAt,
+    updatedAt,
+  };
+  if (name === undefined) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+  books.push(newBooks);
+  const isSuccess = books.filter((book) => book.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
+};
+
+const getAllBooksHandler = () => ({
+  status: 'success',
+  data: {
+    books,
+  },
+});
+
+module.exports = { addBooksHandler, getAllBooksHandler };
--- bookshelf-api/src/routes.js
+++ bookshelf-api/src/routes.js
@@ -1,30 +1,35 @@
 /* eslint linebreak-style: ["error", "windows"] */
+const {
+  addBooksHandler,
+  getAllBooksHandler,
+} = require('./handler');
+
 const routes = [
   {
     method: 'POST',
-    URL: '/books',
-    handler: '',
+    path: '/books',
+    handler: addBooksHandler,
   },
   {
     method: 'GET',
-    URL: '/books',
-    handler: '',
+    path: '/books',
+    handler: getAllBooksHandler,
   },
-  {
-    method: 'POST',
-    URL: '/books/{bookId}',
-    handler: '',
-  },
-  {
-    method: 'PUT',
-    URL: '/books/{bookId}',
-    handler: '',
-  },
-  {
-    method: 'DELETE',
-    URL: '/books/{bookId}',
-    handler: '',
-  },
+  // {
+  //   method: 'POST',
+  //   URL: '/books/{bookId}',
+  //   handler: '',
+  // },
+  // {
+  //   method: 'PUT',
+  //   URL: '/books/{bookId}',
+  //   handler: '',
+  // },
+  // {
+  //   method: 'DELETE',
+  //   URL: '/books/{bookId}',
+  //   handler: '',
+  // },
 ];
 
 module.exports = routes;
