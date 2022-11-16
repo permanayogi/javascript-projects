9add2a9b4701bc5e51e3001225755a536c50b34e
permanayogi <yogipermana597@gmail.com>
2022-11-16 19:11:22 +0700

    detail book handler added

--- bookshelf-api/src/handler.js
+++ bookshelf-api/src/handler.js
@@ -83,4 +83,26 @@ const getAllBooksHandler = () => ({
   },
 });
 
-module.exports = { addBooksHandler, getAllBooksHandler };
+const detailBookHandler = (request, h) => {
+  const { bookId } = request.params;
+
+  const book = books.filter((b) => b.id === bookId)[0];
+
+  if (book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = { addBooksHandler, getAllBooksHandler, detailBookHandler };
--- bookshelf-api/src/routes.js
+++ bookshelf-api/src/routes.js
@@ -2,24 +2,32 @@
 const {
   addBooksHandler,
   getAllBooksHandler,
+  detailBookHandler,
 } = require('./handler');
 
 const routes = [
+
+  // input books data path
   {
     method: 'POST',
     path: '/books',
     handler: addBooksHandler,
   },
+
+  // get all books data path
   {
     method: 'GET',
     path: '/books',
     handler: getAllBooksHandler,
   },
-  // {
-  //   method: 'POST',
-  //   URL: '/books/{bookId}',
-  //   handler: '',
-  // },
+
+  // detail from book path
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: detailBookHandler,
+  },
+
   // {
   //   method: 'PUT',
   //   URL: '/books/{bookId}',
